18fb272791f2c2da6314bcb5fa41f3c2924cecae
Grunewald, BC, Mr [23846348@sun.ac.za] <23846348@sun.ac.za>
2024-05-21 17:46:41 +0200

    Updated run.py to read the rules used in reverse order, extracting the rule names and productions, and filtering out parser junk

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import os
+import sys
+import re
 from lark import Lark
 
 try:
-	#if os.path.exists("parse_history.txt"):
-  os.remove("parse_history.txt")
+    os.remove("parse_history.txt")
 except:
-	print("")
+    pass
+
+rules_used = []
 
 f = open("alan.lark", "r")
 grammar = f.read()
@@ -17,4 +20,21 @@ f = open("test.alan", "r")
 file = f.read()
 f.close()
 
-parser.parse(file)
+parser.parse("source test begin relax end")
+
+print("Rules used:")
+with open("parse_history.txt", 'r') as file:
+    lines = file.readlines()
+start_index = len(lines) - 1
+for i in range(start_index, -1, -1):
+    x = lines[i].rstrip()
+    # check if juck is reached
+    if re.search(r' _[a-zA-Z]', x) or re.search(r'<_[a-zA-Z]', x):
+        break
+    
+    pattern = re.compile(r"<(\w+)\s*:\s*([^>]+)>")
+    match = pattern.search(x)
+    rule = match.group(1)
+    production = match.group(2)
+    print("rule = " + rule + "; production = " + production)
+    rules_used.append((rule, production))
