8e15551ef1c1d75508f3fed7d718c34c88d5bf10
Grunewald, BC, Mr [23846348@sun.ac.za] <23846348@sun.ac.za>
2024-05-21 18:00:34 +0200

    implemented gathering of both the set of rules used during parsing, and a set of all rules (for computing sus_score), and added some documentation

--- run.py
+++ run.py
@@ -8,6 +8,9 @@ try:
 except:
     pass
 
+# list of all rules in parser
+rules = []
+# list of rules used during parse
 rules_used = []
 
 f = open("alan.lark", "r")
@@ -20,21 +23,36 @@ f = open("test.alan", "r")
 file = f.read()
 f.close()
 
-parser.parse("source test begin relax end")
+# gather all exisiting rules
+for x in parser.rules:
+    pattern = re.compile(r"<(\w+)\s*:\s*([^>]+)>")
+    match = pattern.search(str(x))
+    rule = match.group(1)
+    production = match.group(2)
+    rules.insert(0, (rule, production))
 
-print("Rules used:")
+try:
+    parser.parse(file)
+except:
+    pass
+
+# read parse history from parse_history.txt
 with open("parse_history.txt", 'r') as file:
     lines = file.readlines()
 start_index = len(lines) - 1
+# for each entry, in reverse order
 for i in range(start_index, -1, -1):
     x = lines[i].rstrip()
     # check if juck is reached
     if re.search(r' _[a-zA-Z]', x) or re.search(r'<_[a-zA-Z]', x):
         break
-    
+    # get rule and its production used in that step
     pattern = re.compile(r"<(\w+)\s*:\s*([^>]+)>")
     match = pattern.search(x)
     rule = match.group(1)
     production = match.group(2)
-    print("rule = " + rule + "; production = " + production)
-    rules_used.append((rule, production))
+    rules_used.insert(0, (rule, production))
+
+# print rules used
+for x in rules_used:
+    print("rule = " + x[0] + " : production = " + x[1])
