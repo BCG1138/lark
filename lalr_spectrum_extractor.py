d0389daad722c6f21f251b168212223167a9a255
Grunewald, BC, Mr [23846348@sun.ac.za] <23846348@sun.ac.za>
2024-05-22 09:59:13 +0200

    trying to fix jaccard and ochiai

--- lalr_spectrum_extractor.py
+++ lalr_spectrum_extractor.py
@@ -128,9 +128,9 @@ with open("results.txt", 'a') as results:
         tarantula_bottom = tarantula_top + ((vals[0]) / (vals[0] + vals[1] + 0.000000000000000000001))
         tarantula = tarantula_top / tarantula_bottom
         sus_scores[x][0] = tarantula
-        jaccard = (vals[2]) / (vals[2] + vals[3] + vals[0])
+        jaccard = ((vals[2]) / (vals[2] + vals[3] + vals[0]))
         sus_scores[x][1] = jaccard
-        ochiai = (vals[2]) / (math.sqrt((vals[2] + vals[3]) * (vals[2] + vals[0])))
+        ochiai = ((vals[2]) / (math.sqrt((vals[2] + vals[3]) * (vals[2] + vals[0]))))
         sus_scores[x][2] = ochiai
         results.write(str(x) + "\n")
         results.write("\tTarantula: " + str(sus_scores[x][0]) + "\n")
